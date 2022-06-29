58e4cb3d9a7c6eea8cf53999c0e67ee0391cffba
Felipe Carvalho <fcarvalhoosilva@gmail.com>
2022-06-29 14:16:11 -0300

    primeiras funções de clique com js estão ativas

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-pizzaJson.map((item, index)=>{
+const c = (el)=>document.querySelector(el);
+const cs = (el)=>document.querySelectorAll(el);
+
+pizzaJson.map((item, index)=>{ 
     let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
-    document.querySelector('.pizza-area').append(pizzaItem);//passei muito tempo, procurando um erro, e o erro estava, pois esqueci de um ponto...
+    
+    pizzaItem.setAttribute('data-key', index);
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault();
+
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        c('.pizzaBig img').src = pizzaJson[key].img 
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+          c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+        //c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+
+        c('.pizzaWindowArea').style.opacity = 0;
+        c('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(()=>{
+            c('.pizzaWindowArea').style.opacity = 1;
+        }, 200);
+
+
+    });
+
+    c('.pizza-area').append(pizzaItem);
+
     }); 
